e4654df516f9f572dad10caa1f924782e9317667
MR Bednar <M.R.Bednar@gmail.com>
2017-12-01 08:19:33 -0800

    Create Day1 base structure

---
+++ Advent2017/Day1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Advent2017
+{
+    public class Day1 : IDay
+    {
+        public void Run()
+        {
+        }
+    }
+}
