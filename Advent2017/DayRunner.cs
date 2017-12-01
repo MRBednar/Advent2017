bff46fc1331cbea36ff5af1b1a2423b73956b4ff
MR Bednar <M.R.Bednar@gmail.com>
2017-12-01 07:59:52 -0800

    Day 0 setup

---
+++ Advent2017/DayRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent2017
+{
+    class DayRunner
+    {
+        public static void Main(string[] args)
+        {
+            // I wanted a simple way to run whatever day's
+            // code in whatever order I wanted. This takes
+            // the days as inputs and runs them one by one
+            int day;
+            foreach (string arg in args)
+            {
+                if (int.TryParse(arg, out day))
+                {
+                    dayArgument[day].Run();
+                }
+                Console.ReadKey();
+            }
+        }
+
+        public static Dictionary<int, IDay>
+            dayArgument = new Dictionary<int, IDay>
+            {
+
+            };
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent2017
+Solutions for Advent of Code 2017
