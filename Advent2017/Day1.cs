6fc99ef05305f1fa6181aef21788cb2e7b425e9b
MBednar <MBednar@us.imshealth.com>
2017-12-01 18:01:13 -0800

    Day1 Final build. Code prints solution to both part 1 and part 2 for Day 1.

--- Advent2017/Day1.cs
+++ Advent2017/Day1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 
 namespace Advent2017
@@ -10,12 +9,13 @@ namespace Advent2017
         {
             var day1Input = GetDayInput(1);
 
-            int inputTotal = 0;
+            int halfwayTotal = 0;
 
             foreach (string inputLine in day1Input)
             {
                 int lineSum = 0;
                 var charArray = inputLine.ToCharArray();
+                int halfLength = charArray.Length / 2;
                 for (int index = 1; index < charArray.Length; index++)
                 {
 
@@ -31,11 +31,16 @@ namespace Advent2017
                             lineSum = lineSum + int.Parse(charArray[index].ToString());
                         }
                     }
+
+                    if (index < halfLength && charArray[index] == charArray[index + halfLength])
+                    {
+                        Console.WriteLine("Numbers match! Adding {0} to current total of: {1}", charArray[index], halfwayTotal);
+                        halfwayTotal = halfwayTotal + int.Parse(charArray[index].ToString())*2;
+                    }
                 }
                 Console.WriteLine("Line total: {0}",lineSum);
-                inputTotal = inputTotal + lineSum;
             }
-            Console.WriteLine("Input total: {0}", inputTotal);
+            Console.WriteLine("Part 2, the halfway total: {0}", halfwayTotal);
         }
     }
 }
--- Advent2017/IDay.cs
+++ Advent2017/IDay.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Advent2017
+﻿namespace Advent2017
 {
     interface IDay
     {
