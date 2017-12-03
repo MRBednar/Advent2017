d5d9c0ccf3414b6a9e4f3d35b173accd863cb682
mbednar <mbednar@us.imshealth.com>
2017-12-02 22:00:22 -0800

    Day 2 code for part 1

--- Advent2017/Day2.cs
+++ Advent2017/Day2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent2017
 {
@@ -7,7 +8,19 @@ namespace Advent2017
     {
         public override void Run()
         {
+            List<string> numberString = null;
+            List<int> numbers = null;
             var day2Input = GetDayInput(2);
+            int totalChecksum = 0;
+            foreach (string inputLine in day2Input)
+            {
+                string[] inputSplit = { "\t" };
+                numberString = inputLine.Split(inputSplit, StringSplitOptions.RemoveEmptyEntries).ToList();
+                numbers = numberString.Select(i => int.Parse(i)).ToList();
+                numbers.Sort();
+                totalChecksum = totalChecksum + (numbers.Max() - numbers.Min());
+            }
+            Console.WriteLine("Final Total Checksum: {0}", totalChecksum);
         }
     }
 }
