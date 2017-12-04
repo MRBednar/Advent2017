180bb6b1d2926636aafcfd01c3dfc188ce2d55dd
mbednar <mbednar@us.imshealth.com>
2017-12-04 10:36:00 -0800

    Day 3-1 isn't quite working, but here's what I've got

--- Advent2017/Day3.cs
+++ Advent2017/Day3.cs
@@ -8,6 +8,32 @@ namespace Advent2017
         public override void Run()
         {
             var day3Input = GetDayInput(3);
+            double dataGoal = Double.Parse(day3Input[0]);
+            double test = 23;
+            double squareSide = Math.Ceiling(Math.Sqrt(test));
+            if (squareSide % 2 == 0)
+            {
+                squareSide = squareSide + 1;
+                
+            }
+            double sidePos = test - Math.Pow((squareSide - 2), 2);
+            double shortestTaxi = (squareSide - 1) / 2;
+            while (sidePos - squareSide > 0)
+            {
+                sidePos = sidePos - squareSide;
+            }
+            double test2 = 0;
+            if (sidePos > ((squareSide)/2)) {
+                test2 = Math.Ceiling(sidePos % ((squareSide)/2));
+                Console.WriteLine("Dist = {0}", (test2+shortestTaxi));
+            } else if (sidePos < ((squareSide)/2))
+            {
+                test2 = Math.Ceiling(((squareSide)/2) % sidePos);
+                Console.WriteLine("Dist = {0}", (test2 + shortestTaxi));
+            } else
+            {
+                Console.WriteLine("Max dist = {0}", shortestTaxi);
+            }
         }
     }
 }
