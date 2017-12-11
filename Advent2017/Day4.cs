ed4908bb567d31b70c457ed0991491d56d88c4cb
MBednar <MBednar@us.imshealth.com>
2017-12-11 15:10:55 -0800

    Day 4 part 1 complete

---
+++ Advent2017/Day4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent2017
+{
+    public class Day4 : BaseDay
+    {
+        public override void Run()
+        {
+            var day4Input = GetDayInput(4);
+            var validCount = 0;
+            foreach (string inputRow in day4Input)
+            {
+                var validPass = true;
+                List<string> passphrases = new List<string>(inputRow.Split(' '));
+
+                for (int i=0; i< passphrases.Count; i++)
+                {
+                    if (passphrases.LastIndexOf(passphrases[i]) > i)
+                    {
+                        validPass = false;
+                        break;
+                    }
+                }
+
+                if (validPass)
+                {
+                    validCount++;
+                }
+            }
+            Console.WriteLine("Number of valid pasphrases: {0}", validCount);
+        }
+    }
+}
